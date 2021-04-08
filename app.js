87ce866c4a6922b9514c492e76e07e3df0cea5e0
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-04-08 10:34:06 +0300

    added a button but haven't wired it up yet

---
+++ app.js
@@ -0,0 +1,23 @@
+const img = document.querySelector('img');
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=ShwQz1yuFlZwCalQ6bPu2O53jS1GC80E&s=dogs', {
+    mode: 'cors'
+})
+//get the first object as a promise
+  .then(function(response) {
+    return response.json();
+  })
+  //now dig deeper and get the image
+  .then(function(response) {
+   // console.log(response);
+   console.log(response.data.images.original.url);
+   img.src= response.data.images.original.url;
+  });
+
+  //assignment: Add a button
+  const button= document.createElement("button");
+  button.setAttribute("class", "button");
+  button.textContent="Change GIF"
+  
+  //get the container div
+  const container= document.getElementById("container");
+  container.appendChild(button);
