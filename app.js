3de61da2aa88e2307184419e1670b4ca190acba9
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-04-09 10:59:01 +0300

    modified the search function with async await

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // get img element
-const img = document.querySelector("img");
+const img = document.querySelector('img');
 async function getCats() {
   const response = await fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=ShwQz1yuFlZwCalQ6bPu2O53jS1GC80E&s=dogs",
+    'https://api.giphy.com/v1/gifs/translate?api_key=ShwQz1yuFlZwCalQ6bPu2O53jS1GC80E&s=dogs',
     {
-      mode: "cors",
+      mode: 'cors',
     }
   );
   const catData = await response.json();
@@ -13,56 +13,64 @@ async function getCats() {
 getCats();
 
 // assignment: Add a button
-const button = document.createElement("button");
-button.setAttribute("class", "button");
-button.textContent = "Change GIF";
+const button = document.createElement('button');
+button.setAttribute('class', 'button');
+button.textContent = 'Change GIF';
 
 // get the container div
-const container = document.getElementById("container");
+const container = document.getElementById('container');
 container.appendChild(button);
 
 // add an event listener and call the changeGIF function
-button.addEventListener("click", changeGIF);
+button.addEventListener('click', changeGIF);
 
 function changeGIF() {
   fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=ShwQz1yuFlZwCalQ6bPu2O53jS1GC80E&s=cats",
+    'https://api.giphy.com/v1/gifs/translate?api_key=ShwQz1yuFlZwCalQ6bPu2O53jS1GC80E&s=cats',
     {
-      mode: "cors",
+      mode: 'cors',
     }
   )
     .then((response) => response.json())
     .then((response2) => (img.src = response2.data.images.original.url));
 }
 
-const searchBox = document.getElementById("search");
+const searchBox = document.getElementById('search');
 
 // console.log(document.getElementById("search").value);
 // get the search butotn
-const searcBtn = document.getElementById("searchBtn");
+const searcBtn = document.getElementById('searchBtn');
 
 // add an event listener
-searcBtn.addEventListener("click", searchGIF);
+searcBtn.addEventListener('click', searchGIF);
 // get the error para
-const errorPara = document.getElementById("error");
+const errorPara = document.getElementById('error');
 // write the searchGIF function
-function searchGIF() {
+async function searchGIF() {
   // get the search value
-  const searchVal = document.getElementById("search").value;
+  const searchVal = document.getElementById('search').value;
   console.log(searchVal);
 
-  fetch(
+  const response = await fetch(
     `https://api.giphy.com/v1/gifs/translate?api_key=ShwQz1yuFlZwCalQ6bPu2O53jS1GC80E&s=${searchVal}`,
     {
-      mode: "cors",
+      mode: 'cors',
     }
-  )
-    .then((response) => response.json())
-    .then(
-      (response2) => (img.src = response2.data.images.original.url),
-      // erase the error message if it fetches sth
-      (errorPara.textContent = "")
-    )
-    // give error message if the fetch fails
-    .catch((error) => (errorPara.textContent = "Couldn't find that"));
+  );
+  try {
+    const searchData = await response.json();
+    img.src = searchData.data.images.original.url;
+
+    errorPara.textContent = '';
+  } catch (error) {
+    errorPara.textContent = "Couldn't find that";
+  }
 }
+// .then((response) => response.json())
+// .then(
+//   (response2) => (img.src = response2.data.images.original.url),
+//   // erase the error message if it fetches sth
+//   (errorPara.textContent = "")
+// )
+// // give error message if the fetch fails
+// .catch((error) => (errorPara.textContent = "Couldn't find that"));
